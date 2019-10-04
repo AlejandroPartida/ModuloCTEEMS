39c3b820af6348ee93767777e7f2ea183f4e202d
Alejandro Partida <alex.9939@hotmail.com>
2019-10-03 21:53:10 -0500

    Se agregó consultar (equipo José)

---
+++ connection.php
@@ -0,0 +1,17 @@
+<?php
+
+	$servidor="localhost";
+    $user="root";
+    $pass="";
+    $bd="cecyte.sql";
+   /*
+   	$servidor="ejemplo.dominio.com";
+    $bd="congresoft";
+    $user="minombredeusuario";
+    $pass="micontrasena";*/
+
+   $conn=mysqli_connect($servidor,$user,$pass,$bd);
+
+
+
+?>
