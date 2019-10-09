f6613ba1771c1e89b6fa9056c438a7f6139fd5ef
Alejandro Partida <alex.9939@hotmail.com>
2019-10-09 10:57:09 -0500

    Se actualizó la base de datos (9 de octubre)

--- database/cecyte.sql
+++ database/cecyte.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 03, 2019 at 04:55 AM
+-- Generation Time: Oct 07, 2019 at 10:41 PM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.3.4
 
@@ -29,19 +29,14 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `personal` (
+  `id_personal` int(11) NOT NULL,
+  `nombre_personal` varchar(80) COLLATE utf8_spanish2_ci NOT NULL,
   `curp` varchar(18) COLLATE utf8_spanish2_ci NOT NULL,
-  `tel_celular` bigint(11) NOT NULL,
+  `telefono` bigint(10) NOT NULL,
   `correo_electronico` varchar(30) COLLATE utf8_spanish2_ci NOT NULL,
   `puesto` enum('Coordinador','Director','Subdirector') COLLATE utf8_spanish2_ci NOT NULL,
-  `fk_plantel` int(11) NOT NULL
+  `fk_plantel` varchar(10) COLLATE utf8_spanish2_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
-
---
--- Dumping data for table `personal`
---
-
-INSERT INTO `personal` (`curp`, `tel_celular`, `correo_electronico`, `puesto`, `fk_plantel`) VALUES
-('	PXNE660720HMCXTN0', 8666767676, 'jjjjjjshfdgf', 'Coordinador', 1);
 
 -- --------------------------------------------------------
 
@@ -50,7 +45,7 @@ INSERT INTO `personal` (`curp`, `tel_celular`, `correo_electronico`, `puesto`, `
 --
 
 CREATE TABLE `plantel` (
-  `id_plantel` int(11) NOT NULL,
+  `clave_plantel` varchar(10) COLLATE utf8_spanish2_ci NOT NULL,
   `tipo_plantel` enum('CECyTE','EMSaD') COLLATE utf8_spanish2_ci NOT NULL,
   `numero_plantel` int(3) NOT NULL,
   `nombre_plantel` varchar(30) COLLATE utf8_spanish2_ci NOT NULL,
@@ -61,8 +56,8 @@ CREATE TABLE `plantel` (
 -- Dumping data for table `plantel`
 --
 
-INSERT INTO `plantel` (`id_plantel`, `tipo_plantel`, `numero_plantel`, `nombre_plantel`, `telefono_plantel`) VALUES
-(1, 'CECyTE', 19, 'Llano grande', 6188761709);
+INSERT INTO `plantel` (`clave_plantel`, `tipo_plantel`, `numero_plantel`, `nombre_plantel`, `telefono_plantel`) VALUES
+('1', 'CECyTE', 19, 'Llano grande', 6188761709);
 
 --
 -- Indexes for dumped tables
@@ -72,24 +67,14 @@ INSERT INTO `plantel` (`id_plantel`, `tipo_plantel`, `numero_plantel`, `nombre_p
 -- Indexes for table `personal`
 --
 ALTER TABLE `personal`
-  ADD PRIMARY KEY (`curp`),
+  ADD PRIMARY KEY (`id_personal`),
   ADD KEY `fk_plantel` (`fk_plantel`);
 
 --
 -- Indexes for table `plantel`
 --
 ALTER TABLE `plantel`
-  ADD PRIMARY KEY (`id_plantel`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `plantel`
---
-ALTER TABLE `plantel`
-  MODIFY `id_plantel` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  ADD PRIMARY KEY (`clave_plantel`);
 
 --
 -- Constraints for dumped tables
@@ -99,7 +84,7 @@ ALTER TABLE `plantel`
 -- Constraints for table `personal`
 --
 ALTER TABLE `personal`
-  ADD CONSTRAINT `personal_ibfk_1` FOREIGN KEY (`fk_plantel`) REFERENCES `plantel` (`id_plantel`);
+  ADD CONSTRAINT `personal_ibfk_1` FOREIGN KEY (`fk_plantel`) REFERENCES `plantel` (`clave_plantel`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
