6f099a9a4d71944451ac961e1ff48075a7ceb5f5
Salma Gonzalez <salmaglez5@gmail.com>
2019-10-09 15:47:34 -0500

    Equipo 5 a subido el archivo de eliminlineamientos

---
+++ eliminarlin.php
@@ -0,0 +1,19 @@
+<?php
+		
+		require "conexion.php";
+
+		extract($_GET);
+
+		$eliminar = mysqli_query($conn, "DELETE FROM lineamientos WHERE id_lineamiento = 'xd'");
+
+
+		if ($eliminar) {
+			echo "<script>alert('Se ha eliminado el registro'); location.href=''; </script>";
+		}else{
+			echo "<script>alert('Ocurrió un error, inténtelo de nuevo más tarde'); location.href=''; </script>";
+		}
+
+
+
+
+?>
