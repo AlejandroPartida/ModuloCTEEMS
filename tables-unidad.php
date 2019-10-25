43f4ea2f5e6ddfd50c4018e94c6608b2a1585ffb
Juan Pablo <gallardohernandezjuan@hotmail.com>
2019-10-25 14:46:31 -0500

    Se modifico tables-unidad y agrego editarUnidad.js

--- tables-unidad.php
+++ tables-unidad.php
@@ -1,6 +1,4 @@
-<?php include("connection.php"); 
-//conexion
-?>
+<?php include("connection.php");?>
 
 <!doctype html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
